11898b7d503c47c21ee5b23911cad5670266af29
NithinSai11421 <153480138+NithinSai11421@users.noreply.github.com>
2024-12-06 11:45:32 +0530

    2nd Version

--- v1.py
+++ v1.py
@@ -1,6 +1,6 @@
-a = 5
-b = 4
-c = 6
-x = 4.5
+a = float(input("Enter value for a: "))
+b = float(input("Enter value for b: "))
+c = float(input("Enter value for c: "))
+x = float(input("Enter value for x: "))
 y = a*(x*x)+b*x+c
 print("for the values:a",a,"b",b,"c",c,"y is:",y)
