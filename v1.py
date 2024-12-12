539e4ec673ebff784f73b40e7891112b6b336743
NithinSai11421 <153480138+NithinSai11421@users.noreply.github.com>
2024-12-12 21:56:19 +0530

    version 3

--- v1.py
+++ v1.py
@@ -1,6 +1,23 @@
-a = float(input("Enter value for a: "))
-b = float(input("Enter value for b: "))
-c = float(input("Enter value for c: "))
-x = float(input("Enter value for x: "))
-y = a*(x*x)+b*x+c
-print("for the values:a",a,"b",b,"c",c,"y is:",y)
+
+
+with open('i3.txt', 'r') as file:
+    a = int(file.readline().strip())
+    b = int(file.readline().strip())
+    c = int(file.readline().strip())
+    x = int(file.readline().strip())
+
+y = (a * (x ** 2)) + (b * x) + c
+print(f"Value of y: {y}")
+
+
+
+
+
+
+
+
+
+
+
+
+
